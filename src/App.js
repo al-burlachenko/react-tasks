bb30ca3a3223f7e85306e53aa63310376d9314fe
al-burlachenko <alexandrburl@gmail.com>
2025-09-06 15:13:50 +0300

    adds FriendList and TransactionHistory components to App, adds components Transaction, FriendListItem, adds css modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 
 import Profile from "./components/Profile";
+import FriendList from "./components/FriendList";
+import TransactionHistory from "./components/TransactionHistory";
 
 import userData from "./userData.json";
+import friends from "./friends.json";
+import transactions from "./transactions.json";
 
 const App = () => {
   return (
-    <>
+    <div
+      style={{
+        padding: "10px 0",
+        maxWidth: "950px",
+        margin: "0 auto",
+      }}
+    >
       <Profile
         name={userData.username}
         tag={userData.tag}
@@ -14,7 +24,9 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
-    </>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
   );
 };
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,27 +1,25 @@
-import css from "../Profile.css";
-
-console.log(css);
+import css from "../css/Profile.module.css";
 
 export default function Profile(props) {
   return (
-    <div className="profile">
-      <div className="profile-info">
+    <div className={css.profile} key={props.tag}>
+      <div className={css.profileInfo}>
         <img src={props.image} alt="User avatar" />
         <p>{props.name}</p>
         <p>@{props.tag}</p>
         <p>{props.location}</p>
       </div>
 
-      <ul className="profile-statsList">
-        <li className="profile-statsList-item">
+      <ul className={css.profileStatsList}>
+        <li className={css.profileStatsListItem}>
           <span>Followers</span>
           <span>{props.stats.followers}</span>
         </li>
-        <li className="profile-statsList-item">
+        <li className={css.profileStatsListItem}>
           <span>Views</span>
           <span>{props.stats.views}</span>
         </li>
-        <li className="profile-statsList-item">
+        <li className={css.profileStatsListItem}>
           <span>Likes</span>
           <span>{props.stats.likes}</span>
         </li>
